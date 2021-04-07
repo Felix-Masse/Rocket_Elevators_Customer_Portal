31b5ac3e19ffb1b57b317d049625e237d92c86c4
Felix-Masse <flxmasse@hotmail.com>
2021-04-07 14:00:09 -0400

    email

---
+++ RocketElevator_Customer/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RocketElevator_Customer.Areas.Identity.Data;
+using RocketElevator_Customer.Data;
+
+[assembly: HostingStartup(typeof(RocketElevator_Customer.Areas.Identity.IdentityHostingStartup))]
+namespace RocketElevator_Customer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<RocketElevator_CustomerContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("RocketElevator_CustomerContextConnection")));
+
+                services.AddDefaultIdentity<CustomerUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<RocketElevator_CustomerContext>();
+            });
+        }
+    }
+}
